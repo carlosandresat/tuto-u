2495bf60c9aacedd49d4b3e39c706dd11536e674
arielpincay84 <arielpincay84@gmail.com>
2024-01-16 15:57:10 -0500

    Howitworks creation

--- src/app/how/page.tsx
+++ src/app/how/page.tsx
@@ -1,21 +1,26 @@
 import { StartNavbar } from "@/components/start-navbar";
+import Instructions from "@/components/instructions";
+import ScrollElement from "@/components/scroll-to";
 
 export default function How() {
+
   return (
     <>
       <StartNavbar />
 
-      <section className="min-h-screen w-full py-12 flex items-center justify-center flex-col">
-        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight mt-32 md:mt-6 max-w-screen-xl">
-          Cómo funciona
+      <section className="w-full pt-44 flex items-center justify-center flex-col">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-bold tracking-tight max-w-screen-xl">
+          ¿Como usar?
         </h2>
-
-        <div className="flex mt-8">
-        <h1 className="scroll-m-20 text-4xl md:text-6xl font-extrabold tracking-tight lg:text-8xl">
-            Próximamente...
-          </h1>
-
+        <p className="scroll-m-20 text-lg text-muted-foreground mt-4 max-w-screen-md px-8 text-center">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate non corporis assumenda, voluptates culpa quo, suscipit odio ea reprehenderit magnam sit perspiciatis minima aliquam vel necessitatibus. Officiis laudantium ex harum.
+        </p>
+        <div>
+        <ScrollElement id="tutor">
+          <h2 className="underline text-cyan-400 text-2xl text-center cursor-pointer pt-5">Ver para tutores</h2>
+        </ScrollElement>
         </div>
+        <Instructions/>
       </section>
     </>
   );
---
+++ src/components/instructions.tsx
@@ -0,0 +1,112 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/uDCQnoD00vT
+ */
+import { CardTitle, CardDescription, CardContent, Card } from "@/components/ui/card"
+import Image from "next/image"
+
+
+const students_instructions = [
+    {
+        img:'/capt-screen.png',
+        inst:[
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.'
+        ]
+    },
+    {
+        img:'/capt-screen.png',
+        inst:[
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.'
+        ]
+    },
+    {
+        img:'/capt-screen.png',
+        inst:[
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.'
+        ]
+    }
+]
+
+const tutors_instructions = [
+    {
+        img:'/capt-screen.png',
+        inst:[
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.'
+        ]
+    },
+    {
+        img:'/capt-screen.png',
+        inst:[
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.'
+        ]
+    },
+    {
+        img:'/capt-screen.png',
+        inst:[
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.',
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus laudantium error magni, rem ab quibusdam hic maiores atque beatae consectetur asperiores deserunt optio id quas minus, eveniet molestias. Illo, minus.'
+        ]
+    }
+]
+
+function List({inst}:{inst:string[]}){
+    return(
+        inst.map((e,i)=>{
+            return(
+                <div className="flex flex-row gap-2" key={i}>
+                    <h2 className="font-bold text-black dark:text-white">{i+1}.</h2>
+                    <p className="text-gray-500 dark:text-gray-400">{e}</p>
+                </div>
+            )
+        })
+    )
+}
+
+function CardInstruction({img,inst}:{img:string,inst:string[]}){
+    return(
+        <Card className="w-9/10 flex flex-col my-12">
+            <div className="w-full">
+              <Image
+                alt="Instruction Image"
+                className="w-full h-auto"
+                height="642"
+                src={img}
+                style={{
+                  objectFit: "contain",
+                }}
+                width="1340"
+              />
+            </div>
+            <CardContent className="w-full p-6">
+              <CardTitle className="text-lg font-bold">Card Title</CardTitle>
+              <CardDescription className="text-sm">
+                <List inst={inst}/>
+              </CardDescription>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default function Instructions(){
+    return(
+        <div className="w-4/5">
+            <h2 className="text-center text-3xl mt-12 pb-2 border-b md:w-1/4 mx-auto">Para estudiantes:</h2>
+            {students_instructions.map((c,i)=><CardInstruction key={i} img={c.img} inst={c.inst}/>)}
+            <h2 className="text-center text-3xl mt-12 pb-2 border-b md:w-1/4 mx-auto" id="tutor">Para tutores:</h2>
+            {tutors_instructions.map((c,i)=><CardInstruction key={i} img={c.img} inst={c.inst}/>)}
+        </div>
+    )
+}
+
+
