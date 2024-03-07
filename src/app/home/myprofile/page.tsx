afc6dc47bc9c9ddefd12182494968c980a158300
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-03-07 18:45:01 -0500

    feat: initialize myprofile page; add information, signatures and schedules sections

--- src/app/home/myprofile/page.tsx
+++ src/app/home/myprofile/page.tsx
@@ -1,15 +1,180 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  ToggleGroup,
+  ToggleGroupItem,
+} from "@/components/ui/toggle-group"
+import { Car } from "lucide-react";
+
 export default function MyProfile() {
   return (
     <>
-      <section className="min-h-screen w-full py-12 flex items-center justify-center flex-col">
-        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight mt-32 md:mt-6 max-w-screen-xl">
+      <section className="w-full py-12 flex items-center justify-center flex-col">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight pt-32 md:mt-6 max-w-screen-xl">
           Mi Perfil
         </h2>
 
-        <div className="flex mt-8">
-        <h1 className="scroll-m-20 text-4xl md:text-6xl font-extrabold tracking-tight lg:text-8xl">
-            Próximamente...
-          </h1>
+        <div className="flex mt-8 max-w-screen-xl w-full p-8 flex-col">
+          <Card className="w-full">
+            <CardHeader>
+              <Image
+                src="/photos/carlos.arevalo.jpg"
+                alt="Carlos Arévalo"
+                width={96}
+                height={96}
+                className="w-24 h-24 rounded-full"
+              />
+            </CardHeader>
+            <CardContent>
+              <CardTitle>Carlos Arévalo</CardTitle>
+              <CardDescription>
+                <p>
+                  <span className="font-bold">Carrera:</span> Ingeniería en
+                  Tecnologías de la Información
+                </p>
+                <p>
+                  <span className="font-bold">Correo:</span>{" "}
+                  <a href="mailto:carlos.arevalo@yachaytech.edu.ec">carlos.arevalo@yachaytech.edu.ec</a>
+                </p>
+              </CardDescription>
+            </CardContent>
+            <CardFooter>
+              <button className="btn">Editar</button>
+            </CardFooter>
+
+          </Card>
+          <Card className="w-full mt-6">
+            <CardHeader>
+              <CardTitle>Asignaturas como tutor</CardTitle>
+              <CardDescription>Selecciona las asignaturas a las que quieres ser solicitado para tutorías</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ToggleGroup type="multiple" variant="outline">
+                <ToggleGroupItem value="Fisica 1" aria-label="Toggle Física 1"> Física 1 </ToggleGroupItem>
+                <ToggleGroupItem value="Fisica 2" aria-label="Toggle Física 2"> Física 2 </ToggleGroupItem>
+                <ToggleGroupItem value="Matemáticas 1" aria-label="Toggle Matemáticas 1"> Matemáticas 1 </ToggleGroupItem>
+                <ToggleGroupItem value="Matemáticas 2" aria-label="Toggle Matemáticas 2"> Matemáticas 2 </ToggleGroupItem>
+                <ToggleGroupItem value="Matemáticas 3" aria-label="Toggle Matemáticas 3"> Matemáticas 3 </ToggleGroupItem>
+                <ToggleGroupItem value="Matemáticas 4" aria-label="Toggle Matemáticas 4"> Matemáticas 4 </ToggleGroupItem>
+                <ToggleGroupItem value="Matemáticas 5" aria-label="Toggle Matemáticas 5"> Matemáticas 5 </ToggleGroupItem>
+                <ToggleGroupItem value="Matemáticas 6" aria-label="Toggle Matemáticas 6"> Matemáticas 6 </ToggleGroupItem>
+
+              </ToggleGroup>
+            </CardContent>
+            <CardFooter>
+              <Button>Guardar</Button>
+            </CardFooter>
+          </Card>
+          <Card className="w-full mt-6">
+            <CardHeader>
+              <CardTitle>Horarios disponibles</CardTitle>
+              <CardDescription>Selecciona los horarios en los que estás disponible para ofrecer tutorías</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Lunes
+              </h3>
+              <ToggleGroup type="multiple" variant="outline">
+                <ToggleGroupItem value="8am" aria-label="Toggle 8am"> 8am </ToggleGroupItem>
+                <ToggleGroupItem value="10am" aria-label="Toggle 10am"> 10am </ToggleGroupItem>
+                <ToggleGroupItem value="12pm" aria-label="Toggle 2pm"> 12pm </ToggleGroupItem>
+                <ToggleGroupItem value="2pm" aria-label="Toggle 2pm"> 2pm </ToggleGroupItem>
+                <ToggleGroupItem value="4pm" aria-label="Toggle 4pm"> 4pm </ToggleGroupItem>
+                <ToggleGroupItem value="6pm" aria-label="Toggle 6pm"> 6pm </ToggleGroupItem>
+                <ToggleGroupItem value="8pm" aria-label="Toggle 8pm"> 8pm </ToggleGroupItem>
+                <ToggleGroupItem value="10pm" aria-label="Toggle 10pm"> 10pm </ToggleGroupItem>
+              </ToggleGroup>
+              <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Martes
+              </h3>
+              <ToggleGroup type="multiple" variant="outline">
+                <ToggleGroupItem value="8am" aria-label="Toggle 8am"> 8am </ToggleGroupItem>
+                <ToggleGroupItem value="10am" aria-label="Toggle 10am"> 10am </ToggleGroupItem>
+                <ToggleGroupItem value="12pm" aria-label="Toggle 2pm"> 12pm </ToggleGroupItem>
+                <ToggleGroupItem value="2pm" aria-label="Toggle 2pm"> 2pm </ToggleGroupItem>
+                <ToggleGroupItem value="4pm" aria-label="Toggle 4pm"> 4pm </ToggleGroupItem>
+                <ToggleGroupItem value="6pm" aria-label="Toggle 6pm"> 6pm </ToggleGroupItem>
+                <ToggleGroupItem value="8pm" aria-label="Toggle 8pm"> 8pm </ToggleGroupItem>
+                <ToggleGroupItem value="10pm" aria-label="Toggle 10pm"> 10pm </ToggleGroupItem>
+              </ToggleGroup>
+              <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Miércoles
+              </h3>
+              <ToggleGroup type="multiple" variant="outline">
+                <ToggleGroupItem value="8am" aria-label="Toggle 8am"> 8am </ToggleGroupItem>
+                <ToggleGroupItem value="10am" aria-label="Toggle 10am"> 10am </ToggleGroupItem>
+                <ToggleGroupItem value="12pm" aria-label="Toggle 2pm"> 12pm </ToggleGroupItem>
+                <ToggleGroupItem value="2pm" aria-label="Toggle 2pm"> 2pm </ToggleGroupItem>
+                <ToggleGroupItem value="4pm" aria-label="Toggle 4pm"> 4pm </ToggleGroupItem>
+                <ToggleGroupItem value="6pm" aria-label="Toggle 6pm"> 6pm </ToggleGroupItem>
+                <ToggleGroupItem value="8pm" aria-label="Toggle 8pm"> 8pm </ToggleGroupItem>
+                <ToggleGroupItem value="10pm" aria-label="Toggle 10pm"> 10pm </ToggleGroupItem>
+              </ToggleGroup>
+              <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Jueves
+              </h3>
+              <ToggleGroup type="multiple" variant="outline">
+                <ToggleGroupItem value="8am" aria-label="Toggle 8am"> 8am </ToggleGroupItem>
+                <ToggleGroupItem value="10am" aria-label="Toggle 10am"> 10am </ToggleGroupItem>
+                <ToggleGroupItem value="12pm" aria-label="Toggle 2pm"> 12pm </ToggleGroupItem>
+                <ToggleGroupItem value="2pm" aria-label="Toggle 2pm"> 2pm </ToggleGroupItem>
+                <ToggleGroupItem value="4pm" aria-label="Toggle 4pm"> 4pm </ToggleGroupItem>
+                <ToggleGroupItem value="6pm" aria-label="Toggle 6pm"> 6pm </ToggleGroupItem>
+                <ToggleGroupItem value="8pm" aria-label="Toggle 8pm"> 8pm </ToggleGroupItem>
+                <ToggleGroupItem value="10pm" aria-label="Toggle 10pm"> 10pm </ToggleGroupItem>
+              </ToggleGroup>
+              <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Viernes
+              </h3>
+              <ToggleGroup type="multiple" variant="outline">
+                <ToggleGroupItem value="8am" aria-label="Toggle 8am"> 8am </ToggleGroupItem>
+                <ToggleGroupItem value="10am" aria-label="Toggle 10am"> 10am </ToggleGroupItem>
+                <ToggleGroupItem value="12pm" aria-label="Toggle 2pm"> 12pm </ToggleGroupItem>
+                <ToggleGroupItem value="2pm" aria-label="Toggle 2pm"> 2pm </ToggleGroupItem>
+                <ToggleGroupItem value="4pm" aria-label="Toggle 4pm"> 4pm </ToggleGroupItem>
+                <ToggleGroupItem value="6pm" aria-label="Toggle 6pm"> 6pm </ToggleGroupItem>
+                <ToggleGroupItem value="8pm" aria-label="Toggle 8pm"> 8pm </ToggleGroupItem>
+                <ToggleGroupItem value="10pm" aria-label="Toggle 10pm"> 10pm </ToggleGroupItem>
+              </ToggleGroup>
+              <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Sábado
+              </h3>
+              <ToggleGroup type="multiple" variant="outline">
+                <ToggleGroupItem value="8am" aria-label="Toggle 8am"> 8am </ToggleGroupItem>
+                <ToggleGroupItem value="10am" aria-label="Toggle 10am"> 10am </ToggleGroupItem>
+                <ToggleGroupItem value="12pm" aria-label="Toggle 2pm"> 12pm </ToggleGroupItem>
+                <ToggleGroupItem value="2pm" aria-label="Toggle 2pm"> 2pm </ToggleGroupItem>
+                <ToggleGroupItem value="4pm" aria-label="Toggle 4pm"> 4pm </ToggleGroupItem>
+                <ToggleGroupItem value="6pm" aria-label="Toggle 6pm"> 6pm </ToggleGroupItem>
+                <ToggleGroupItem value="8pm" aria-label="Toggle 8pm"> 8pm </ToggleGroupItem>
+                <ToggleGroupItem value="10pm" aria-label="Toggle 10pm"> 10pm </ToggleGroupItem>
+              </ToggleGroup>
+              <h3 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Domingo
+              </h3>
+              <ToggleGroup type="multiple" variant="outline">
+                <ToggleGroupItem value="8am" aria-label="Toggle 8am"> 8am </ToggleGroupItem>
+                <ToggleGroupItem value="10am" aria-label="Toggle 10am"> 10am </ToggleGroupItem>
+                <ToggleGroupItem value="12pm" aria-label="Toggle 2pm"> 12pm </ToggleGroupItem>
+                <ToggleGroupItem value="2pm" aria-label="Toggle 2pm"> 2pm </ToggleGroupItem>
+                <ToggleGroupItem value="4pm" aria-label="Toggle 4pm"> 4pm </ToggleGroupItem>
+                <ToggleGroupItem value="6pm" aria-label="Toggle 6pm"> 6pm </ToggleGroupItem>
+                <ToggleGroupItem value="8pm" aria-label="Toggle 8pm"> 8pm </ToggleGroupItem>
+                <ToggleGroupItem value="10pm" aria-label="Toggle 10pm"> 10pm </ToggleGroupItem>
+              </ToggleGroup>
+            </CardContent>
+            <CardFooter>
+              <Button>Guardar</Button>
+            </CardFooter>
+          </Card>
 
         </div>
       </section>
--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -21,7 +21,7 @@ const ToggleGroup = React.forwardRef<
 >(({ className, variant, size, children, ...props }, ref) => (
   <ToggleGroupPrimitive.Root
     ref={ref}
-    className={cn("flex items-center justify-center gap-1", className)}
+    className={cn("items-center justify-center gap-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5", className)}
     {...props}
   >
     <ToggleGroupContext.Provider value={{ variant, size }}>
