1458c42d82c1a554bfcb88f8ff6f36f4d9040e93
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-05-04 11:03:45 -0500

    feat: Authjs initialize, middleware configuration

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+export const publicRoutes = [
+    "/",
+    "/faq",
+    "/how",
+    "/ourteam",
+    "/preregister",
+    "/pricing"
+];
+
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register",
+];
+
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = "/home"
