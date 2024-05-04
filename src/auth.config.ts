9ff90221e9f9e2f57a1ca15b1d3dae5a5eee17d5
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-05-04 16:05:40 -0500

    feat: add authjs credentials provider to config

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,3 +1,36 @@
 import type { NextAuthConfig } from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { LoginSchema } from "@/schemas"
+import bcrypt from "bcryptjs"
+import { db } from "@/lib/db"
 
-export default { providers: [] } satisfies NextAuthConfig
+export default { 
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const validatedFields = LoginSchema.safeParse(credentials);
+
+                if (validatedFields.success) {
+                    const { email, password } = validatedFields.data;
+
+                    const user = await db.user.findUnique({
+                        where: {
+                            email,
+                        },
+                    });
+
+                    if (!user || !user.password) return null;
+
+                    const passwordsMatch = await bcrypt.compare(
+                        password,
+                        user.password
+                    )
+
+                    if (passwordsMatch) return user;
+
+                }
+                return null;
+            },
+        })
+    ] 
+} satisfies NextAuthConfig
