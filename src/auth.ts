9433b7ab3aaf769f255796408f50bc4793c46537
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-05-05 16:18:32 -0500

    feat: add callbacks for extend userId in session

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,17 @@ import {db} from "@/lib/db"
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session
+    },
+    jwt({ token }) {
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt"},
   ...authConfig
