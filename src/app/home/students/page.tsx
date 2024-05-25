3ec7ee92ac639f94c591c7016584357063c32241
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-05-25 16:46:34 -0500

    feat: add /students page with AllStudents component

--- src/app/home/students/page.tsx
+++ src/app/home/students/page.tsx
@@ -1,19 +1,12 @@
+import { AllStudents } from "@/components/all-students";
 
 export default function Students() {
   return (
-    <>
-      <section className="min-h-screen w-full py-12 flex items-center justify-center flex-col">
-        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight mt-32 md:mt-6 max-w-screen-xl">
+      <section className="flex min-h-screen flex-col items-center justify-between p-8">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight max-w-screen-xl pt-32">
           Estudiantes
         </h2>
-
-        <div className="flex mt-8">
-        <h1 className="scroll-m-20 text-4xl md:text-6xl font-extrabold tracking-tight lg:text-8xl">
-            Próximamente...
-          </h1>
-
-        </div>
+        <AllStudents />
       </section>
-    </>
   );
 }
