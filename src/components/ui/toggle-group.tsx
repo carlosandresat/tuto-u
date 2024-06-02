4de67b8be1193bc7134506228b5030b7f6f1f4e8
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-06-02 15:14:36 -0500

    style: update focus effect on ToggleGroupItem

--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -47,7 +47,8 @@ const ToggleGroupItem = React.forwardRef<
           variant: context.variant || variant,
           size: context.size || size,
         }),
-        className
+        className,
+        "focus:bg-transparent"
       )}
       {...props}
     >
