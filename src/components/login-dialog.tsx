98d3c991a761bcda550c7b20c10e1e6896b3937a
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-06-04 14:57:47 -0500

    feat: add restart password dialog for step by step info

--- src/components/login-dialog.tsx
+++ src/components/login-dialog.tsx
@@ -2,11 +2,14 @@ import {
     Dialog,
     DialogTrigger,
     DialogContent,
+    DialogDescription,
     DialogHeader,
     DialogTitle,
   } from "@/components/ui/dialog";
   import { Button } from "@/components/ui/button";
   import { LoginForm } from "@/components/login-form";
+  import Link from "next/link";
+  import Image from "next/image";
 
   
   export function LoginDialog({buttonText}:{buttonText:string}) {
@@ -21,7 +24,29 @@ import {
           <DialogHeader>
             <DialogTitle>Ingresa tus credenciales</DialogTitle>
           </DialogHeader>
-          <LoginForm></LoginForm>  
+          <LoginForm></LoginForm>
+          <Dialog>
+          <div className="flex justify-center">
+            <DialogTrigger asChild>
+              <Button variant="link">¿Olvidaste la contraseña?</Button>
+            </DialogTrigger>
+          </div>
+
+          <DialogContent className="sm:max-w-2xl">
+            <DialogHeader>
+              <DialogTitle>Reestablece tu contraseña</DialogTitle>
+              <DialogDescription>Aún no terminamos de implementar esta funcionalidad, por favor sigue estos pasos y actualizaremos manualmente tu contraseña</DialogDescription>
+            </DialogHeader>
+            <div className="flex justify-center">
+            <Image
+                src={`/instructions/restart-password.png`}
+                alt="Restart password instructions"
+                width={900}
+                height={900}
+              />
+              </div>
+          </DialogContent>
+        </Dialog>
         </DialogContent>
       </Dialog>
     );
