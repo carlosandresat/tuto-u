3e2fe263e694d3e010626f8f5f779a35047ee477
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-06-19 01:07:38 -0500

    feat: update middleware redirect when not logged and not public route

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,7 +29,7 @@ export default auth((req) => {
     }
 
     if (!isLoggedIn && !isPublicRoute){
-        return Response.redirect(new URL("/", nextUrl))
+        return Response.redirect(new URL("/auth/login", nextUrl))
     }
 
     return
