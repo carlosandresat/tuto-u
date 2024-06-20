9f6a3e446268ee3cd777442bdd07a865391d43bc
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-06-20 10:57:18 -0500

    feat: add how to done instructions and footer in OurTeam page

--- src/app/ourteam/page.tsx
+++ src/app/ourteam/page.tsx
@@ -9,6 +9,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { ModeToggle } from "@/components/mode-toggle";
 
 import Link from "next/link";
 import { Github } from "lucide-react";
@@ -241,7 +244,7 @@ export default function OurTeam() {
           </Card>
         </div>
       </section>
-      <section className="pt-12 pb-12 w-full flex items-center justify-center flex-col">
+      <section className="pt-12 pb-6 w-full flex items-center justify-center flex-col">
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight max-w-screen-xl">
           Nuestros Donantes
         </h2>
@@ -331,7 +334,45 @@ export default function OurTeam() {
             </div>
           </div>
         </div>
+        <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="link" className="text-md">¿Quieres apoyarnos? Averigua cómo aquí</Button>
+            </DialogTrigger>
+
+            <DialogContent className="sm:max-w-2xl">
+              <DialogHeader>
+                <DialogTitle>Gracias por tu apoyo</DialogTitle>
+                <DialogDescription>
+                  Dona desde $1 y contáctanos por nuestras redes para añadirte en esta sección. Tu donación ayuda mucho a los desarrolladores y el mantenimiento de la aplicación.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="flex justify-center">
+                <Image
+                  src={`/instructions/howdonate.png`}
+                  alt="Restart password instructions"
+                  width={900}
+                  height={900}
+                />
+              </div>
+            </DialogContent>
+          </Dialog>
+          
       </section>
+      <footer className="flex flex-col gap-2 md:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <p className="text-xs text-gray-500 dark:text-gray-400 text-center">
+          © Tuto-U. Desarrollado por{" "}
+          <span className="font-bold text-foreground">Tuto-U Team</span>.
+        </p>
+        <nav className="md:ml-auto flex gap-4 md:gap-6 mr-6">
+          <Link className="text-xs hover:underline underline-offset-4" href="#">
+            Términos y condiciones
+          </Link>
+          <Link className="text-xs hover:underline underline-offset-4" href="#">
+            Privacidad
+          </Link>
+        </nav>
+        <ModeToggle></ModeToggle>
+      </footer>
     </>
   );
 }
