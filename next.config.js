ade23bb48401a86987371f6a501cc485ca798627
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-06-20 23:19:37 -0500

    chore: add remotePatterns for images in nextConfig file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gajrsoefybqm4ylo.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
