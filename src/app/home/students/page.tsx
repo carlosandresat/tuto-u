99928199d59303e23c019fa11b4631a08981814f
Carlos Arevalo <carlosandresat@hotmail.com>
2024-12-20 15:41:36 -0500

    fix: remove justify-between from Students and Tutors sections pages

--- src/app/home/students/page.tsx
+++ src/app/home/students/page.tsx
@@ -2,7 +2,7 @@ import { AllStudents } from "@/components/all-students";
 
 export default function Students() {
   return (
-      <section className="flex min-h-screen flex-col items-center justify-between p-8">
+      <section className="flex min-h-screen flex-col items-center p-8">
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight max-w-screen-xl pt-32">
           Estudiantes
         </h2>
--- src/app/home/tutors/page.tsx
+++ src/app/home/tutors/page.tsx
@@ -2,7 +2,7 @@ import { AllTutors } from "@/components/all-tutors";
 
 export default function Tutors() {
   return (
-      <section className="flex min-h-screen flex-col items-center justify-between p-8">
+      <section className="flex min-h-screen flex-col items-center p-8">
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight max-w-screen-xl pt-32">
           Tutores
         </h2>
