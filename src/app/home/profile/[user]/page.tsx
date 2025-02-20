5951944f3cbbefc6210f5296de06bf0de0e09522
Carlos Arevalo <carlosandresat@hotmail.com>
2025-02-20 00:00:06 -0500

    feat: enable dynamic metadata generation for user profiles

--- src/app/home/profile/[user]/page.tsx
+++ src/app/home/profile/[user]/page.tsx
@@ -4,25 +4,18 @@ type Props = {
   params: { user: string };
 };
 
-/*export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   // read route params
   const user = params.user;
   const title = `${user} | Tuto-U`;
   const description = `Visita el perfil de ${user}. Conoce su progreso en Tuto-U.`;
-  const page_url = `https://tutou.app/home/profile/${user}`
   // fetch data
 
   return {
-    metadataBase: new URL(page_url),
     title: title,
     description: description,
   };
-}*/
-
-export const metadata: Metadata = {
-    title: "¡Visita mi perfil!",
-    description: "Conoce toda mi información en Tuto-U",
-  };
+}
 
 export default function Page({ params }: Props) {
   return (
