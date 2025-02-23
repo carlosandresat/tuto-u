8436c7f0843dabb128bfbf945f92e77ae697cb97
Carlos Arevalo <carlosandresat@hotmail.com>
2025-02-23 00:30:15 -0500

    fix: update GET function signature to include request parameter for user name retrieval

--- src/app/api/profile/[email]/route.ts
+++ src/app/api/profile/[email]/route.ts
@@ -1,6 +1,11 @@
 import { getUserNameByEmail } from "@/actions/profile";
 
-export async function GET({ params }: { params: { email: string } }) {
+export async function GET({
+  params,
+}: {
+  request: Request;
+  params: { email: string };
+}) {
   const name = await getUserNameByEmail(params.email);
   return new Response(name, { status: 200 });
 }
