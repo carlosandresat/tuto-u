9ff8a67546c427964030bf79624bc29e3f0e7f72
Carlos Arevalo <carlosandresat@hotmail.com>
2025-02-24 16:58:19 -0500

    feat: add function to retrieve user tag by ID

--- src/actions/profile.ts
+++ src/actions/profile.ts
@@ -82,3 +82,23 @@ export const getUserNameByEmail = async (email: string) => {
     return "Not Found User";
   }
 };
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: { id },
+      select: {
+        email: true,
+      },
+    });
+
+    if (!user || !user.email) {
+      return "not-found";
+    }
+
+    return user.email.split("@")[0].replace(".", "-")
+  } catch (error) {
+    console.error("Failed to fetch user profile:", error);
+    return "not-found";
+  }
+};
