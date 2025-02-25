b5f4ad14bd7b73a618a433bbc5b444769da419cb
Carlos Arevalo <carlosandresat@hotmail.com>
2025-02-25 17:00:19 -0500

    feat: update user request page to include dynamic profile link to return

--- src/app/[user]/request/page.tsx
+++ src/app/[user]/request/page.tsx
@@ -10,11 +10,11 @@ import {
 } from "@/components/ui/card";
 import { ProfileRequestForm } from "@/components/profile-request-form";
 
-export default function Page() {
+export default function Page({ params }: { params: { user: string } }) {
   return (
     <section className="md:min-h-screen w-full p-6 items-center flex flex-col space-y-6">
       <Button variant="link" className="px-0 self-start" asChild>
-        <Link href="#">
+        <Link href={`/${params.user}/profile`}>
           <ArrowLeft className="mr-2" /> Regresar
         </Link>
       </Button>
