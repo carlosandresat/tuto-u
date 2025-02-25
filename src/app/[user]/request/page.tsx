029b2cf4ef79a8ee9382e81210c80b94e9439615
Carlos Arevalo <carlosandresat@hotmail.com>
2025-02-24 23:34:32 -0500

    feat: add layout for user request page with navigation button and card component

--- src/app/[user]/request/page.tsx
+++ src/app/[user]/request/page.tsx
@@ -1,7 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  Card,
+} from "@/components/ui/card";
+
 export default function Page() {
   return (
     <section className="md:min-h-screen w-full p-6 items-center flex flex-col space-y-6">
-      
+      <Button variant="link" asChild>
+        <Link href="#" className="self-start">
+          <ArrowLeft className="mr-2" /> Regresar
+        </Link>
+      </Button>
+      <Card className="w-full max-w-screen-lg">
+        <CardHeader>
+          <CardTitle>Solicita una tutoría con Nombre Tutor</CardTitle>
+          <CardDescription>
+            Llena el formulario para realizar una solicitud de tutoría
+          </CardDescription>
+        </CardHeader>
+        <CardContent>Aquí irá el formulario</CardContent>
+      </Card>
     </section>
   );
 }
