d4c4bee82642de971c559c49a40b0a11209f0d28
Carlos Arevalo <carlosandresat@hotmail.com>
2025-03-06 16:52:13 -0500

    fix: change HTML language attribute from English to Spanish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
         <link
           rel="apple-touch-icon"
